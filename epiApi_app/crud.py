a6a3f5da2428b45fcb25307c63e25feef1c39b3f
ian <ian@DESKTOP-RCHQUBT.localdomain>
2022-12-26 13:58:19 -0500

    remove username from user Table

--- epiApi_app/crud.py
+++ epiApi_app/crud.py
@@ -5,7 +5,6 @@ def get_users(db: Session):
     return db.query(models.User).all()
 
 def get_user(db: Session, user_id: int):
-
     return db.query(models.User).filter(models.User.id == user_id).first()
 
 def create_user(db: Session, user: schemas.User):
